28360ce579c5c1733d9d978286cdfefbf87a6793
Jamie Morrissey <jmo1344@wgu.edu>
2025-11-15 13:50:33 -0800

    got HTML table working

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
 	
 	private AuthenticationService authenticationService = new AuthenticationService();
@@ -25,7 +27,7 @@ public class LoginController {
 	public String gotoWelcomePage(@RequestParam String name, 
 			@RequestParam String password, ModelMap model) {
 		model.put("name", name);
-		
+	
 		 //Authentication Logic
 		if(authenticationService.authenticate(name, password)) {
 		
