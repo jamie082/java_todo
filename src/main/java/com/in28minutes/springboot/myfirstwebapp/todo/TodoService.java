d4ebf5aee700e2fcbb43ce3b1a543bf01dbf13d0
Jamie Morrissey <jmo1344@wgu.edu>
2025-11-17 17:42:05 -0800

    got datepicker working

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -7,13 +7,15 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TodoService {
 	private static List<Todo> todos = new ArrayList<>();
 	private static int todosCount = 0;
 	
 	static {
-		todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "Get AWS Certified", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack", LocalDate.now().plusYears(3), false));
 	}
@@ -31,5 +33,18 @@ public class TodoService {
 		Predicate<? super Todo> predicate
 			= todo -> todo.getId() == id;
 		todos.removeIf(predicate);
+	}
+	
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		// TODO Auto-generated method stub
+		deleteById(todo.getId());
+		todos.add(todo);
+		
 	}
 }
