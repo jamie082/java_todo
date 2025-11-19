948e2108c3c70e7e7761486b83ce1b43a81fa10a
Jamie Morrissey <jmo1344@wgu.edu>
2025-11-18 21:51:56 -0800

    didn't add the H2 database

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -4,11 +4,13 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -21,9 +23,13 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
 				
-		String username = "in28minutes";
-		String password = "dummy";
-		
+		UserDetails userDetails1 = createNewUser("in28minutes", "dummy");		
+		UserDetails userDetails2 = createNewUser("ranga", "dummydummy");
+
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String username, String password) {
 		Function<String, String> passwordEncoder
 		= input -> passwordEncoder().encode(input);
 		
@@ -33,8 +39,7 @@ public class SpringSecurityConfiguration {
 				.password(password)
 				.roles("USER", "ADMIN")
 				.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
 	}
 	
 	@Bean
